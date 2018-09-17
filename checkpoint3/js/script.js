0bda228f31ee187fd37862d1df281c7f10948105
dkroll713 <dkroll713@gmail.com>
2018-09-17 13:04:42 -0500

    checkpoint3

--- checkpoint3/js/script.js
+++ checkpoint3/js/script.js
@@ -1,9 +1,7 @@
 "use strict";
 
 $(document).ready(function(){
-  var count1 = document.getElementById('count1').text;
-  console.log(count1);
-  $('#pu1').on('click', function(){
-    count1++;
-  })
+  $("#pu1").on("click", function({
+    $("#count1").t
+  }))
 })
