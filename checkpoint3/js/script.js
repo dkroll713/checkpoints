fa84a040a1eef9b1b2d93e79712d234f4b9086a7
dkroll713 <dkroll713@gmail.com>
2018-09-17 17:04:31 -0500

    checkpoint3

--- checkpoint3/js/script.js
+++ checkpoint3/js/script.js
@@ -1,9 +1,20 @@
 "use strict";
 
 $(document).ready(function(){
-  $(this).on("click", function(){
-    var $counter1 = $("#count1");
-    console.log($($counter1).text());
-    $($counter1).text() == ($($counter1).text() + 1);
-  })
-})
+
+var $counter1 = $("#count1").text();
+var $counter2 = $("#count2").text();
+var $counter3 = $("#count3").text();
+var $counter4 = $("#count4").text();
+var $counter5 = $("#count5").text();
+  $("#btn1").click(function(){
+    $("#count1").text() == ($("#count1").text() + 1);
+    console.log($counter1);
+  });
+
+  $("#btn2").click(function(){
+    document.getElementById("count2").innerHTML == $counter2++;
+    console.log($counter2);
+  });
+
+});
